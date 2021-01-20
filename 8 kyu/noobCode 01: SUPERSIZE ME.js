b6d85ca93626d13bb93ecd1815a93448b891eca0
prullansky <owenchboy@gmail.com>
2021-01-20 12:56:53 +0100

    added 8kyu kata

---
+++ 8 kyu/noobCode 01: SUPERSIZE ME.js
@@ -0,0 +1,19 @@
+/*
+
+https://www.codewars.com/kata/5709bdd2f088096786000008
+
+Write a function that rearranges an integer into its largest possible value.
+
+superSize(123456) //654321
+superSize(105) // 510
+superSize(12) // 21
+If the argument passed through is single digit or is already the maximum possible integer, your function should simply return it.
+
+
+*/
+
+function superSize(num){
+
+    return parseInt(num.toString().split('').sort().reverse().join(''))
+    
+    }
