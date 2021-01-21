5b89bbd511784bc42eb40bee206c5ddca4f08773
prullansky <owenchboy@gmail.com>
2021-01-21 16:25:32 +0100

    added new 6kyu kata

---
+++ 6 kyu/Take a Ten Minute Walk.js
@@ -0,0 +1,43 @@
+/*
+
+https://www.codewars.com/kata/54da539698b8a2ad76000228
+
+You live in the city of Cartesia where all roads are laid out in a perfect grid. 
+You arrived ten minutes too early to an appointment, so you decided to take the opportunity 
+to go for a short walk. The city provides its citizens with a Walk Generating App on their 
+phones -- everytime you press the button it sends you an array of one-letter strings representing 
+directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each 
+letter (direction) and you know it takes you one minute to traverse one city block, so create 
+a function that will return true if the walk the app gives you will take you exactly ten minutes 
+(you don't want to be early or late!) and will, of course, return you to your starting point. 
+Return false otherwise.
+
+Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). 
+It will never give you an empty array (that's not a walk, that's standing still!).
+
+
+*/
+
+function isValidWalk(walk) {
+
+    let score = {x:0, y:0}
+    
+    if (walk.length === 10) {
+    for (let i = 0; i < walk.length; i++) {
+    if (walk[i] === 'n') {
+    score.y += 1;
+    } else if (walk[i] === 'e') {
+    score.x += 1;
+    } else if (walk[i] === 'w') {
+    score.x -= 1;
+    } else if (walk[i] === 's') {
+    score.y -= 1;
+    }
+    }
+    }
+    if (score.x === 0 && score.y === 0 && walk.length === 10) {
+    return true
+    } else {
+    return false
+    }
+    }
