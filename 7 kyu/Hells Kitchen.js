4068b61e4a8ae5629c8e4abf026afedb880f43ef
prullansky <owenchboy@gmail.com>
2021-02-10 09:37:18 +0100

    added new 6kyu and 7kyu kata

---
+++ 7 kyu/Hells Kitchen.js
@@ -0,0 +1,25 @@
+/*
+
+https://www.codewars.com/kata/57d1f36705c186d018000813
+
+
+Gordon Ramsay shouts. He shouts and swears. There may be something wrong with him.
+
+Anyway, you will be given a string of four words. Your job is to turn them in to Gordon language.
+
+Rules:
+
+Obviously the words should be Caps, Every word should end with '!!!!', Any letter 'a' or 'A' should become '@', Any other vowel should become '*'.
+
+*/
+
+function gordon(a){
+
+    return a.toUpperCase()
+    .replace(/A/g, '@')
+    .replace(/[E,I,O,U]/g, '*')
+    .split(' ')
+    .map(word => word + '!!!!')
+    .join(' ')
+    
+    }
